29ee2f8f34221edb551ae7d43cce5a49c446582d
Bill Meany <billedatat@gmail.com>
2019-08-05 11:10:26 -0400

    Update CopyQB.py
    Starting to add comments and play with file operations.

--- CopyQB.py
+++ CopyQB.py
@@ -5,4 +5,46 @@ This is a simple utility program to backup specific files, in this case we
 are backing up our Quicken database.
 """
 
+from os import *
+from io import *
 from shutil import *
+
+QbSflag = False
+QbTflag = False
+
+try:
+    parmFile = open("CopyQB.parms","r",1)
+except PermissionError:
+    print('Access permission error')
+except FileNotFoundError:
+    print('File does not exist or not found')
+else:
+    isReadable = parmFile.readable()
+
+
+for lines in parmFile:
+
+  myLines = lines.split("=",2)
+  
+  if myLines[0].strip() == "QbSrc" :
+    QbSource = myLines[1].strip()
+    QbSflag = True
+	
+  if myLines[0].strip() == "QbTgt" :
+    QbTarget = myLines[1].strip()
+    QbTflag = True	
+  
+parmFile.close()
+
+# Trivial test to make sure we have the two parameters we need.
+
+if QbSflag == False or QbTflag == False :
+  print('Parameter error in a parm file record.')
+  exit()
+
+print(QbSource)
+print(QbTarget)
+
+#Need to get pyAesCrypt so we can encrypt files prior to placing on cloud
+#Grab it from pypi.org
+

8667ec471d00adf4b1a6d75ba2889669b9e29a7e
Bill Meany <billedatat@gmail.com>
2019-08-17 08:30:43 -0400

    Update CopyQB.py
    Added more comments. Also using pyAesCrypt to encrypt the output files. Added a new parameter to parm file for the password we use with the encryption.

--- CopyQB.py
+++ CopyQB.py
@@ -1,32 +1,44 @@
 # CopyQB.py
+# Version 1.0
 
-"""
-This is a simple utility program to backup specific files, in this case we
-are backing up our Quicken database.
-"""
+# This is a simple utility program to backup specific files, in this case we
+# are backing up our Quicken database. The backup copies are going to be 
+# encrypted.
+
+# Copy in needed support code
 
 from os import *
-from io import *
-from shutil import *
-from stat import *
+from io import open
 from filecmp import dircmp
+from pyAesCrypt import encryptFile
+
+# Define and initialize some variables we will use.
 
 sourceFile = ''
 targetFile = ''
 QbSflag = False
 QbTflag = False
+QbPflag = False
 SDfiles = []
 TDfiles = []
+kByte = 1024
+bufferSize = 64 * kByte
+
+# See if we can open the parameter file.
 
 try:
     parmFile = open("CopyQB.parms","r",1)
 except PermissionError:
     print('Access permission error')
+    exit()
 except FileNotFoundError:
     print('File does not exist or not found')
+    exit()
 else:
     isReadable = parmFile.readable()
 
+# Read and process the parameter file. Very simple logic to isolate the
+# parameters we need.
 
 for lines in parmFile:
 
@@ -38,13 +50,17 @@ for lines in parmFile:
 	
   if myLines[0].strip() == "QbTgt" :
     QbTpath = myLines[1].strip()
-    QbTflag = True	
+    QbTflag = True
+
+  if myLines[0].strip() == "QbPwd" :
+    QbPassword = myLines[1].strip()
+    QbPflag = True	
   
 parmFile.close()
 
-# Trivial test to make sure we have the two parameters we need.
+# Trivial test to make sure we have the three parameters we need.
 
-if QbSflag == False or QbTflag == False :
+if QbSflag == False or QbTflag == False or QbPflag == False :
   print('Parameter error in a parm file record.')
   exit()
 
@@ -66,16 +82,11 @@ if not path.isdir(QbTpath) :
 filesNeedingBackup = dircmp(QbSpath,QbTpath,None,None).left_only
 
 # Now that we have the list of files we can iterate over them and back them
-# up using the copy2 method from shutil.
+# up using the encryptFile method drom pyAesCrypt.
 
 for FNB in filesNeedingBackup :
   sourceFile = QbSpath + FNB
-  targetFile = QbTpath + FNB
-  copy2(sourceFile, targetFile)
-
-
-
-
-#Need to get pyAesCrypt so we can encrypt files prior to placing on cloud
-#Grab it from pypi.org
-
+  targetFile = QbTpath + FNB + '.aes'
+  encryptFile(sourceFile, targetFile, QbPassword, bufferSize)
+  
+exit()

12c9faf70e3c492f663f078d099e3ef42183a8cd
Bill Meany <billedatat@gmail.com>
2019-09-23 09:29:29 -0400

    Update sfbkup.py
    Added version indicator.

--- sfbkup.py
+++ sfbkup.py
@@ -1,5 +1,8 @@
 # sfbkup.py
-# Version 1.0
+# Version 1.00
+# Version 1.01
+# Version 1.02
+# Version 1.03
 
 # Python program to create backups of files. The list of files to be backed
 # is specified in the sfbkup.parms file. This file contains a list of direc-
@@ -10,6 +13,7 @@
 
 from os import *
 from sys import setrecursionlimit
+from subprocess import call
 from io import open
 from shutil import copy2
 from filecmp import dircmp
@@ -22,7 +26,7 @@ from datetime import datetime
 
 def open_the_logfile(otl_tuple) :
 
-  localList = list(otl_tuple)	
+  localList = list(otl_tuple)   
   LogFileLocation = localList[0]
   LogFileNamePrefix = localList[1]  
   
@@ -40,6 +44,11 @@ def open_the_logfile(otl_tuple) :
   else:
     return LogFileDescriptor
 
+#
+# write_to_logfile is a simple routine to allow us to write standardized mes-
+# sages to a logging file.
+#
+
 def write_to_logfile(wtl_tuple) :
 
     fileDescriptor = wtl_tuple[0]
@@ -49,36 +58,79 @@ def write_to_logfile(wtl_tuple) :
     logMessage = timeStamp + ' ' + messageText
     fileDescriptor.write(logMessage)
 
-    return True	
-	
+    return True
+
+# enum_directory is a simple funtion that is used to locate and save
+# directory entries.
+#
+# We call the routine with a tuple that contains two entries. The first entry
+# is the list of the source directories that we are building. The second entry
+# is the current entry we are examining.
+#
+# We use the try/except/else logic block to catch any errors that may occur.
+# We should never encounter the NotADirectoryError, but it is used as a safety.
+# We are more likely to encounter the PermissionError as we may be executing
+# the script as a normal user and not and administrator.
+#
+# There is most likely a more elegant way to obtain the list of directories and
+# subdirectories, but I have started with this method as I am trying to learn
+# Python.
+#
+# Note that were calling the routine recusively.
+    
 def enum_directory(ed_tuple) :
 
     localList = list(ed_tuple)
     sdList = localList[0]
     currentDirect = localList[1]
-
-    with scandir(currentDirect) as localDirectory :
+    
+    try :
+      localDirectory = scandir(currentDirect)
+    except NotADirectoryError :
+      return False
+    except PermissionError :
+      return False
+    else :
+      sdList.append(currentDirect)
       for localEntry in localDirectory :
-        if not localEntry.name.startswith('.') and localEntry.is_dir():    
-          sdList.append(localEntry)
-          localTuple = (sdList, localEntry)
+        if localEntry.is_dir():         
+          localTuple = (sdList, localEntry.path)
           localFlag = enum_directory(localTuple)
-		  
+          
       return True
+
+# Set our recursion limit/depth.
+
+setrecursionlimit(10000)
 
 # Define and initialize some variables we will use.
 
 sourceFile = ''
 targetFile = ''
 logHandle = ''
+backupPrefix = 'D:\Asus SyncFolder\@BU\\'
+
+QbEflag = False
 QbSflag = False
 QbLflag = False
 EnumFlag = False
+
 baseDirect = []
+baseExcept = []
 sourceDirect = []
 targetDirect = []
 sourceFiles = []
 targetFiles = []
+
+sourceFile = []
+sourcePath = []
+sourceMtime = []
+sourceSize = []
+
+targetFile = []
+targetPath = []
+targetMtime = []
+targetSize = []
 
 # See if we can open the parameter file.
 
@@ -104,15 +156,19 @@ for lines in parmFile:
     QbSpath = myLines[1].strip()
     QbSflag = True
 
+  if myLines[0].strip() == "BkExc" :
+    QbEpath = myLines[1].strip()
+    QbEflag = True
+
   if myLines[0].strip() == "BkLfl" :
     QbLogFileLoc = myLines[1].strip()
-    QbLflag = True	
+    QbLflag = True  
   
 parmFile.close()
 
 # Trivial test to make sure we have the two parameters we need.
 
-if (QbSflag == False or QbLflag == False) :
+if (QbEflag == False or QbSflag == False or QbLflag == False) :
   print('Parameter error in a parm file record.')
   exit()
   
@@ -127,8 +183,14 @@ myTuple = (logHandle, 'Beginning program execution.\n')
 QbLflag = write_to_logfile(myTuple)
 myTuple = (logHandle, 'Processing records from source file.\n')
 QbLflag = write_to_logfile(myTuple)
- 
 
+#
+# Next we will process the source directory file. This file contains the base
+# set of directories we will start with. If we are able to open the file then
+# we will read the files contents, and then close the file.
+# 
+
+linesRead = 0
 try:
     BkSrc = open(QbSpath,"r",1)
 except PermissionError:
@@ -139,11 +201,44 @@ except FileNotFoundError:
     exit()
 else:
     isReadable = BkSrc.readable()
-	
+    
 for lines in BkSrc :
   baseDirect.append(lines.strip())
+  linesRead += 1
   
 BkSrc.close()
+
+logMessage = 'Number of base directories = ' + str(linesRead) +'\n'
+myTuple = (logHandle, logMessage)
+QbLflag = write_to_logfile(myTuple)
+
+#
+# Next we will process the exclusion list file. These are known directories
+# that we do not want to attempt to backup. If we are able to open the
+# file, we will read the file contents and then close the file.
+#
+
+linesRead = 0
+try:
+    BkExc = open(QbEpath,"r",1)
+except PermissionError:
+    print('Access permission error')
+    exit()
+except FileNotFoundError:
+    print('File does not exist or not found')
+    exit()
+else:
+    isReadable = BkExc.readable()
+    
+for lines in BkExc :
+  baseExcept.append(lines.strip())
+  linesRead += 1
+  
+BkExc.close()
+
+logMessage = 'Number of directories to exclude = ' + str(linesRead) +'\n'
+myTuple = (logHandle, logMessage)
+QbLflag = write_to_logfile(myTuple)
 
 # At this point sourceDirect contains the base list of directories that we will
 # examine for backup opportunities. This will be our main loop for building the
@@ -153,64 +248,164 @@ BkSrc.close()
 # turn a boolean flag.
 
 for SD in baseDirect :
-    sourceDirect.append(SD)
     myTuple = (sourceDirect, SD)
     EnumFlag = enum_directory(myTuple)
+    
+#
+# Now that we have the total list of directories that are to be backed up,
+# we will remove any direcory that was in the exclude list.
+#
 
-print(sourceDirect[15])
-dList = scandir(SD)
+for ED in baseExcept :
+    try :
+      sourceDirect.remove(ED)
+    except ValueError :
+      pass
 
-for xyzzz in dList :
- print(xyzzz)
+#
+# At this point we have located all of the source directories that we will pro-
+# cess. We sort the list of directories. Next we create the matching list of
+# target directories that we will back files up to.
+#
+
+logMessage = 'Total number of directories to process = ' + str(len(sourceDirect)) +'\n'
+myTuple = (logHandle, logMessage)
+QbLflag = write_to_logfile(myTuple)
+
+sourceDirect.sort()
+
+myTuple = (logHandle, 'Source directory list has been sorted.\n')
+QbLflag = write_to_logfile(myTuple)
+
+
+# Now create the list of target directories.
+
+sourceTotal = 0
+
+for SD in sourceDirect :
+    splitSD = SD.split('\\', 1)
+    BD = backupPrefix + splitSD[1]
+    targetDirect.append(BD)
+    sourceTotal += 1  
+
+targetTotal = sourceTotal
   
-print(len(sourceDirect))
-exit()
+myTuple = (logHandle, 'Target directory list has been built.\n')
+QbLflag = write_to_logfile(myTuple)
+myTuple = (logHandle, 'Checking for the existence of target directories.\n')
+QbLflag = write_to_logfile(myTuple)
 
-if not path.isdir(QbSpath) :
-  errMsg = QbSpath + ' is not a directory, exiting routine\n'
-  myTuple = (logHandle, errMsg)
-  QbLflag = write_to_logfile(myTuple)
-  exit()
-else :
-  txtMsg = 'Source directory is ' + QbSpath + '\n'
-  logHandle.write(txtMsg)
-  
-if not path.isdir(QbTpath) :
-  errMsg = QbTpath + ' is not a directory, exiting routine\n'
-  logHandle.write(errMsg)
-  exit()
-else :
-  txtMsg = 'Target directory is ' + QbTpath + '\n'
-  logHandle.write(txtMsg)
-  
-txtMsg = 'Source and target directories are valid\n'
-logHandle.write(txtMsg)
+#
+# Next we will iterate through the list of target directories directories to
+# make sure they all exist. If we get a FileNotFoundError we will go ahead and
+# create the target directory.
+#
 
-txtMsg = 'Obtaining list of files in source and not in target\n'
-logHandle.write(txtMsg)
+for TD in targetDirect :
+#    print(TD, '\n')
+    try :
+      scandir(TD)
+    except FileNotFoundError :
+      mkdir(TD)
+      logMessage = 'Directory created = ' + TD +'\n'
+      myTuple = (logHandle, logMessage)
+      QbLflag = write_to_logfile(myTuple)
 
-# We are going to use the dircmp function from filecmp. This will do a
-# comparison of the source and target directories. From that we will use
-# the left_only property to obtain a list of files that are in the source
-# directory, but not in the target directory.
+myTuple = (logHandle, 'Completed checking for target directories.\n')
+QbLflag = write_to_logfile(myTuple)
 
-filesNeedingBackup = dircmp(QbSpath,QbTpath,None,None).left_only
+#
+# The following is the main loop for determining if an individual file needs
+# to be backed up. We do this by going through all of the source and matching
+# target directories looking for files that are on the source and not on the
+# target, or files on the source and target with the same name but different
+# date/time stamps and/or file size.
+#
 
-# Now that we have the list of files we can iterate over them and back them
-# up using the copy2 method.
+sourcePointer = 0
+totalFilesBackedUp = 0
+totalBytes = 0
 
-for FNB in filesNeedingBackup :
-  sourceFile = QbSpath + FNB
-  targetFile = QbTpath + FNB
-  try:
-    copy2(sourceFile, targetFile)
-  except OSError:
-    print("Error")
-  else:
-    txtMsg = sourceFile + ' has been copied to target directory\n'
-    logHandle.write(txtMsg)
+for sourcePointer in range(sourceTotal) :
 
-logHandle.write('Exiting program\n')  
-logHandle.close()
+    sdFlag = False
+    try:
+      sourceEntries = scandir(sourceDirect[sourcePointer])
+    except NotADirectoryError :
+      pass
+    except PermissionError :
+      pass
+    else :
+      sdFlag = True
+      seCounter = 0
+      for SE in sourceEntries :
+        if SE.is_file() :
+          sourceFile.append(SE.name)
+          sourcePath.append(SE.path)
+          statVals = SE.stat()
+          sourceMtime.append(statVals.st_mtime)
+          sourceSize.append(statVals.st_size)
+          sdFlag = True
+          seCounter += 1
 
+    if sdFlag : 
+      tdFlag = False
+      try:
+        targetEntries = scandir(targetDirect[sourcePointer])
+      except NotADirectoryError :
+        pass
+      except PermissionError :
+        pass
+      else :
+        tdFlag = True
+        for TE in targetEntries :
+          if TE.is_file() :
+            targetFile.append(TE.name)
+            targetPath.append(TE.path)
+            statVals = TE.stat()
+            targetMtime.append(statVals.st_mtime)
+            targetSize.append(statVals.st_size)
+      
+      if sdFlag and tdFlag :
+        for sfPointer in range(seCounter) :
+          try :
+            tfPointer = targetFile.index(sourceFile[sfPointer])
+          except ValueError :
+            sourceFileEntry = sourcePath[sfPointer]
+            splitSD = sourceFileEntry.split('\\',1)
+            targetFileEntry = backupPrefix + splitSD[1]
+            logMessage = 'Backing up --> ' + sourceFileEntry + '\n'
+            myTuple = (logHandle, logMessage)
+            QbLflag = write_to_logfile(myTuple)
+            copy2(sourceFileEntry, targetFileEntry)
+            totalFilesBackedUp += 1
+            totalBytes = totalBytes + sourceSize[sfPointer]
+          else :
+            if ((sourceMtime[sfPointer] > targetMtime[sfPointer]) or
+              (sourceSize[sfPointer] != targetSize[sfPointer])) :
+                sourceFileEntry = sourcePath[sfPointer]
+                targetFileEntry = targetPath[sfPointer]
+                logMessage = 'Backing up ++> ' + sourceFileEntry + '\n'
+                myTuple = (logHandle, logMessage)
+                QbLflag = write_to_logfile(myTuple)
+                copy2(sourceFileEntry, targetFileEntry)            
+                totalFilesBackedUp += 1
+                totalBytes = totalBytes + sourceSize[sfPointer]
+    
+    sourceEntries.close()
+    targetEntries.close()
+
+logMessage = 'Total number of files backed up = ' + str(totalFilesBackedUp) + '\n'
+myTuple = (logHandle, logMessage)
+QbLflag = write_to_logfile(myTuple)
+
+if totalBytes != 0 :
+    totalBytes = totalBytes / 1024
+
+logMessage = 'Total number of Kbytes backed up = ' + str(totalBytes) + '\n'
+myTuple = (logHandle, logMessage)
+QbLflag = write_to_logfile(myTuple) 
+myTuple = (logHandle, 'Terminating program execution \n')
+QbLflag = write_to_logfile(myTuple)
+    
 exit()
